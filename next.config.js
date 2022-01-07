84b50f586345ba6b79d0bbbeebfff90f7b74e3fe
bguillen <bguillen@truechoice.io>
2022-01-07 18:24:16 -0500

    chore: migrate config from bitbucket

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const withTM = require('next-transpile-modules')(['@truechoice-solutions/ui']);
+
+module.exports = withTM({
+  target: 'serverless',
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/game/:path*',
+        destination: `http://localhost/game/:path*`
+      }
+    ];
+  }
+});
